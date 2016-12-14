8df8f55b864326979ce59abe4784ca752f7ede00
MiraFox <noc@mirafox.ru>
2016-12-14 15:23:05 +0300

    add RUNPODS

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,10 +3,16 @@
 set -e
 
 TIMEZONE=${TIMEZONE:-Europe/Moscow}
+RUNPODS=${RUNPODS:-Off}
 
 # set timezone
 echo ${TIMEZONE} | tee /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
+
+if [ ${RUNPODS} == 'On' ]; then
+    sed -i "s/myapp:9000;/127.0.0.1:9000;/g" /etc/nginx/conf.d/default.conf
+    sed -i "s/myapp:9000;/127.0.0.1:9000;/g" /etc/nginx/conf.d/default-ssl.conf
+fi
 
 if [ -f /var/www/html/config/nginx/nginx.conf ]; then
     cp /var/www/html/config/nginx/nginx.conf /etc/nginx/nginx.conf
